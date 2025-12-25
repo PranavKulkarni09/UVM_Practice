d6f876ea20af85b73f542a2e94c69ecf4c969a46
PranavKulkarni09 <mr.pranav.kulkarni.s@gmail.com>
2025-12-24 22:23:43 -0800

    Create solution.sv

---
+++ Assignment3:A13/solution.sv
@@ -0,0 +1,11 @@
+module tb;
+  initial begin
+    uvm_top.set_report_max_quit_count(4);
+    uvm_top.set_report_severity_action(UVM_WARNING, UVM_COUNT | UVM_DISPLAY);
+  end
+  initial begin
+    for(int i = 0; i<10; i++) begin
+      `uvm_warning("TB", $sformatf("Random message num: %0d", $urandom_range(0,10)));
+    end
+  end
+endmodule
