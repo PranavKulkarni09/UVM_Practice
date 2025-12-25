860bb0f636a05bb597591f9f5387d68b81c08d57
PranavKulkarni09 <mr.pranav.kulkarni.s@gmail.com>
2025-12-25 11:54:49 -0800

    Create solution.sv

---
+++ Assignment4:A14/solution.sv
@@ -0,0 +1,26 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+class component extends uvm_component;
+  `uvm_component_utils(component)
+  
+  function new(string path , uvm_component parent);
+    super.new(path, parent);
+  endfunction
+ 
+  
+  task run();
+    `uvm_info("CMP1", "Executed CMP1 Code", UVM_DEBUG);
+    `uvm_info("CMP2", "Executed CMP2 Code", UVM_DEBUG);
+  endtask
+  
+endclass
+
+module tb;
+  component c;
+  initial begin
+    c = new("c", null);
+    c.set_report_id_verbosity("CMP1", UVM_DEBUG);
+    c.run();
+  end
+endmodule
